95e43ebd03bfd7cc10a2046a2464fecdb9de0c1e
Kevin Opoku-Agyemang <kevino.agyemang@gmail.com>
2023-11-14 19:19:16 -0500

    remove console.log

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -9,7 +9,6 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
 };
-console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
