4ec4d28f8910993dc0d394ebc08921351480f18f
Kevin Opoku-Agyemang <kevino.agyemang@gmail.com>
2024-02-04 19:37:49 -0500

    trigger workflow

--- Packages/vite-react-app/src/Pages/LoginPage/Login.tsx
+++ Packages/vite-react-app/src/Pages/LoginPage/Login.tsx
@@ -34,6 +34,7 @@ export const Login: FC<LoginProps> = () => {
         alignItems: "center",
       }}
     >
+      <div></div>
       <Button sx={{ backgroundColor: "blue" }} onClick={signIn}>
         Sign In
       </Button>
