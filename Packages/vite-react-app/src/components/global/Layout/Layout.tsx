5150f171be1057ec6e92596417e443b7ba913ebf
Kevin Opoku-Agyemang <kevino.agyemang@gmail.com>
2024-03-09 12:04:02 -0500

    all changes

--- Packages/vite-react-app/src/components/global/Layout/Layout.tsx
+++ Packages/vite-react-app/src/components/global/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from "react";
 import "./index.scss";
 import { Sidebar } from "../Sidebar/Sidebar";
+import { Header } from "../Header";
 
 type LayoutProps = {
   header?: ReactNode;
@@ -10,31 +11,24 @@ type LayoutProps = {
 
 export const Layout: FC<LayoutProps> = ({ body }) => {
   return (
-    <section className="animated-grid">
-      <Sidebar className="side-nav" />
-      {/* <div className="search_header">{header}</div> */}
-      <div className="body-content">{body}</div>
-    </section>
-    // <Container maxWidth={false} disableGutters>
-    //   <Box sx={{ height: "100vh", display: "flex", flexDirection: "column" }}>
-    //     {/* Header */}
-
-    //     {/* Body */}
-    //     <Box sx={{ flexGrow: 1, width: "100%" }}>
-    //       <Paper elevation={1} sx={{ height: "100%", overflow: "auto" }}>
-    //         <Box sx={{ width: "100%" }}>{header}</Box>
-    //         <Box p={2}>{body}</Box>
-    //       </Paper>
-    //     </Box>
-
-    //     {/* Footer */}
-    //     <Box sx={{ width: "100%" }}>
-    //       <Paper elevation={1}>
-    //         <Box p={2}>{footer}</Box>
-    //       </Paper>
-    //     </Box>
-    //   </Box>
-    // </Container>
+    <>
+      <div className="app-container_wrapper">
+        <Header />
+        <section className="main-body_container">
+          <aside>
+            <Sidebar />
+          </aside>
+          <div className="body-content">{body}</div>
+        </section>
+      </div>
+      {/* <Header />
+      <section className="animated-grid">
+        <aside>
+          <Sidebar className="side-nav" />
+        </aside>
+        <div className="body-content">{body}</div>
+      </section> */}
+    </>
   );
 };
 
--- Packages/vite-react-app/src/components/global/Sidebar/Sidebar.tsx
+++ Packages/vite-react-app/src/components/global/Sidebar/Sidebar.tsx
@@ -1,76 +1,11 @@
-import { FC, useState } from "react";
-import Drawer from "@mui/material/Drawer";
-import List from "@mui/material/List";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import { ListItemButton } from "@mui/material";
+import React, { FC } from "react";
 
-const drawerWidth = 300;
-
-type SidebarProps = {
-  className: string;
+export type SidebarProps = {
+  // Define your props here
 };
 
-export const Sidebar: FC<SidebarProps> = ({ className }) => {
-  const [selectedIndex, setSelectedIndex] = useState<number | undefined>(1);
-
-  const handleListItemClick = (index: number | undefined) => {
-    if (index) {
-      setSelectedIndex(index);
-    }
-  };
-
-  return (
-    <Drawer
-      className={`${className}`}
-      data-testid="sidebar-test-class"
-      sx={{
-        width: drawerWidth,
-        border: "none",
-        flexShrink: 0,
-        "& .MuiDrawer-paper": {
-          width: drawerWidth,
-          backgroundColor: "#32baae;",
-          borderWidth: 0,
-        },
-      }}
-      variant="permanent"
-      anchor="left"
-    >
-      <List sx={{ paddingTop: "2rem" }}>
-        {["Home", "All Students", "Connections", "Chat"].map((text, index) => (
-          <div key={index + 1}>
-            <ListItemButton
-              style={{ width: "300px" }}
-              className={`menu-item ${
-                selectedIndex === index + 1 ? "selected" : ""
-              }`}
-              key={index + 1}
-              selected={selectedIndex === index + 1}
-              onClick={() => handleListItemClick(index + 1)}
-            >
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-              {/* {selectedIndex === index && (
-                <div
-                  style={{
-                    backgroundColor: "white",
-                    height: "5rem",
-                    width: "30rem",
-                    position: "absolute",
-                    left: "200px",
-                    borderRadius: "50px",
-                  }}
-                ></div>
-              )} */}
-            </ListItemButton>
-          </div>
-        ))}
-      </List>
-    </Drawer>
-  );
+export const Sidebar: FC<SidebarProps> = () => {
+  return <div className="bg-blue-500 text-white p-4">Hello, Tailwind!</div>;
 };
+
+export default Sidebar;
