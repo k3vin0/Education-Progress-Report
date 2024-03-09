4705cc687d4852f853d2e06930eb0adb7d4f4267
Kevin Opoku-Agyemang <kevino.agyemang@gmail.com>
2024-03-09 14:05:07 -0500

    Remove previous next app

---
+++ src/components/global/Sidebar/Sidebar.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+import NavItems, { NavigationItem } from "./components/NavItems/NavItems";
+
+export type SidebarProps = {
+  navigationItem: NavigationItem[];
+  onClick: (link: string) => void;
+};
+
+export const Sidebar: FC<SidebarProps> = ({ navigationItem, onClick }) => {
+  return (
+    <div className="flex flex-col w-60  h-full border-2 border-slate-500">
+      {navigationItem.map((navItem) => (
+        <NavItems
+          navigationItem={navItem}
+          onClick={
+            onClick
+              ? () => onClick(navItem.link ? navItem.link : "")
+              : undefined
+          }
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/global/Sidebar/components/NavItems/NavItems.tsx
@@ -0,0 +1,58 @@
+import React, { FC, ReactNode, useState } from "react";
+
+export type NavigationItemChildren = {
+  label: string;
+  link?: string;
+  icon?: ReactNode;
+};
+export type NavigationItem = {
+  label: string;
+  link?: string;
+  icon?: ReactNode;
+  children?: NavigationItemChildren[];
+};
+
+export type NavItemsProps = {
+  navigationItem: NavigationItem;
+  onClick?: (link: string) => void;
+};
+
+export const NavItems: FC<NavItemsProps> = ({ navigationItem, onClick }) => {
+  const { label, link, children, icon } = navigationItem;
+  const [showChildren, setShowChildren] = useState<boolean>(false);
+  return (
+    <>
+      <div
+        className="w-full h-15 hover:bg-teal-300 p-4"
+        onClick={
+          link && onClick
+            ? () => {
+                onClick(link), children && setShowChildren(!showChildren);
+              }
+            : () => children && setShowChildren(!showChildren)
+        }
+      >
+        <div className="flew flex-row items-center content-center ">
+          <div className="flex flex-row items-center content-center gap-2">
+            {icon}
+            <span>{label}</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        {showChildren && (
+          <div>
+            {children?.map((child) => (
+              <div className="w-full h-15 hover:bg-teal-300 p-4 flex flex-row items-center content-center gap-2">
+                <div>{child.icon}</div>
+                <div>{child.label}</div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default NavItems;
