f4be1554253f17ae8da7f9b8e0052c4ec866a2b5
Kevin Opoku-Agyemang <kevino.agyemang@gmail.com>
2024-03-09 14:11:05 -0500

    trigger deploy

--- src/components/global/Header/Header.tsx
+++ src/components/global/Header/Header.tsx
@@ -8,7 +8,11 @@ export const Header: FC<HeaderProps> = () => {
   return (
     <div className="w-full h-16 flex flex-row">
       <div className="w-2/12">Icon</div>
-      <div className="w-10/12">content</div>
+      <div className="w-10/12">
+        <span>content</span>
+        <span>content</span>
+        <span>content</span>
+      </div>
     </div>
   );
 };
